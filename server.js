36ddb5790a834df31ce23d8ad37c23efd4a1b394
Amin <min123kabir@gmail.com>
2024-10-29 00:40:43 +0100

    try

--- server.js
+++ server.js
@@ -23,8 +23,11 @@ const db = new pg.Client({
   host: process.env.PG_HOST,
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT
-})
+  port: process.env.PG_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+    required: true,
+  },
 let count = 0
 
 
